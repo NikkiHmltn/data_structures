3fe4984052b9b43c00f8f51e76ade458699e3a9a
Nicole Hamilton <Nicole@NicolesLaptop.local>
2021-01-12 20:58:34 -0800

    added stack and queue

---
+++ queue.py
@@ -0,0 +1,12 @@
+# a queue can be a list
+queue = []
+
+queue.append('One')
+queue.append('Two')
+queue.append('Three')
+
+print(queue)
+
+#remove elements from the queue
+print(queue.pop(0))
+print(queue)
---
+++ stacks.py
@@ -0,0 +1,14 @@
+# a stack can also be a list
+stack = []
+
+stack.append('Ten')
+stack.append('Twenty')
+stack.append('30')
+
+print(stack)
+
+# remove elements as a stack
+print(stack.pop())
+print(stack.pop())
+
+print(stack)
